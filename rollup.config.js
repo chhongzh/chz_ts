e19aa5ef7455baabe78ed0d5067a4535b393f82c
chhongzh <2550519316@qq.com>
2025-04-09 19:09:25 +0800

    fix: 修复打包问题

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 // rollup.config.js
-import json from '@rollup/plugin-json'
-import typescript from '@rollup/plugin-typescript'
-import pkg from './package.json' with {type: "json"}
+import json from '@rollup/plugin-json';
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json' with { type: "json" };
+
 export default {
   input: 'src/main.ts',
   output: [
@@ -13,6 +14,6 @@ export default {
   ],
   plugins: [
     json(),
-    typescript({lib: [ "es6", "dom"], target: "es6"})
+    typescript({ lib: ["es6", "dom"], target: "es6" })
   ]
 };
